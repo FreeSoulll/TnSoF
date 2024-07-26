437573d52f6c7a86082727995ab95fa54b04a8cc
FreeSoulll <greg.kr9@gmail.com>
2024-07-26 11:51:30 +0300

    change webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
 import '../stylesheets/application.scss'
+import 'bootstrap'
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
 Rails.start()
 Turbolinks.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery'
+  })
+)
+
 module.exports = environment
